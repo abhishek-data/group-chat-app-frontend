7cf105727d569b7702d93a8bdcc66451e0f4ec97
Abhishek Kumar <abhishekkr1915@gmail.com>
2023-11-28 20:37:33 +0530

    chat page created

--- src/components/chatPage/index.jsx
+++ src/components/chatPage/index.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react';
+import { Input, Button, List, Avatar } from 'antd';
+
+const { TextArea } = Input;
 
 const ChatPage = () => {
-  return (
-    <div>ChatPage</div>
-  )
-}
+  const [messages, setMessages] = useState([]);
+  const [newMessage, setNewMessage] = useState('');
+  const messageListRef = useRef(null)
 
-export default ChatPage
+  useEffect(() => {
+    if (messageListRef.current) {
+      messageListRef.current.scrollTop = messageListRef.current.scrollHeight;
+    }
+  }, [messages])
+
+
+  const handleSendMessage = () => {
+    if (newMessage.trim() === '') {
+      return;
+    }
+
+    const updatedMessages = [...messages, { text: newMessage, sender: 'You' }];
+    setMessages(updatedMessages);
+    setNewMessage('');
+  };
+
+  return (
+    <div className="chat-container">
+      <div className="message-list" ref={messageListRef}>
+        <List
+          itemLayout="horizontal"
+          dataSource={messages}
+          renderItem={(message, index) => (
+            <List.Item>
+              <List.Item.Meta
+                avatar={<Avatar>{message.sender[0]}</Avatar>}
+                title={message.sender}
+                description={message.text}
+              />
+            </List.Item>
+          )}
+        />
+      </div>
+
+      <div className="input-container">
+        <TextArea
+          rows={2}
+          value={newMessage}
+          onChange={(e) => setNewMessage(e.target.value)}
+          onPressEnter={handleSendMessage}
+          className="textarea"
+        />
+        <Button type="primary" onClick={handleSendMessage} className="button">
+          Send
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default ChatPage;
